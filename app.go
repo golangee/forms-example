58081d72d67477397bdb13c9c9bc88e8b0ef345c
Torben Schinke <torben.schinke@worldiety.de>
2020-03-27 18:01:47 +0100

    wtk api experiments

--- app.go
+++ app.go
@@ -1,6 +1,10 @@
 package example
 
-import "log"
+import (
+	"github.com/worldiety/wtk"
+	"log"
+	"strconv"
+)
 
 type App struct {
 }
@@ -9,7 +13,41 @@ func NewApp() *App {
 	return &App{}
 }
 
+type MyCustomComponent struct {
+	text1   *wtk.Text
+	text2   *wtk.Text
+	counter int
+}
+
+func NewMyCustomComponent() *MyCustomComponent {
+	c := &MyCustomComponent{}
+
+	vstack := &wtk.VStack{}
+
+	c.text1 = &wtk.Text{}
+	c.text1.Value.Set("hello world")
+	vstack.AddView(c.text1)
+
+	c.text2 = &wtk.Text{}
+	c.text2.Value.Set("a second text line")
+	vstack.AddView(c.text2)
+
+	btn := &wtk.Button{Text: "press me"}
+	btn.AddOnClickListener(func() {
+		c.counter++
+		c.text2.Value.Set("pressed " + strconv.Itoa(c.counter))
+	})
+	vstack.AddView(btn)
+	return c
+}
+
 func (a *App) Run() {
 	log.Println("wasm done4")
+
+	wtk.Root.RemoveAll()
+
+	myView := NewMyCustomComponent()
+	wtk.Root.AddView(myView)
+
 	select {}
 }
--- go.mod
+++ go.mod
@@ -4,4 +4,6 @@ go 1.14
 
 replace github.com/worldiety/wtk => ../wtk
 
+require github.com/worldiety/wtk v0.0.0
+
 require github.com/lpar/gzipped/v2 v2.0.0-rc1
