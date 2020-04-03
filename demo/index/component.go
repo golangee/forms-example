69676429b4ec997acb1695991461200b8c2a6f1c
Torben Schinke <torben.schinke@worldiety.de>
2020-04-03 18:33:12 +0200

    added goals and non-goals

--- demo/index/component.go
+++ demo/index/component.go
@@ -17,7 +17,18 @@ func NewContentView() *ContentView {
 		NewText("wtk").Style(Font(Headline1)).Style(PadBottom(8)),
 		NewText("welcome to the wtk demo and kitchen sink. Here you can see "+
 			"a selected amount of use cases and components of the worldiety web toolkit "+
-			"for go and wasm.").Style(Font(Body)),
+			"for go and wasm. It should feel a bit like SwiftUI but for Go.").Style(Font(Body)),
+		NewText("goals").Style(Font(Headline2)),
+		NewText("Provide an 80% solution for rapid prototyping and "+
+			"digitization of companies. The UI must be state of the art "+
+			"and working across desktop and mobile, in modern Webkit and Firefox "+
+			"Browsers. Best suited for form based web apps.").Style(Font(Body)),
+		NewText("non-goals").Style(Font(Headline2)),
+		NewText("Developing bleeding edge and fully customizable html "+
+			"applications. Compatibility with older browsers or SEO "+
+			"are unimportant. Everything should be expressed in a declarative way "+
+			"without the need of external Javascript libraries.").Style(Font(Body)),
+		NewText("chapters").Style(Font(Headline2)),
 		NewButton("buttons").AddClickListener(func(v View) {
 			v.Context().Navigate(button.Path)
 		}).Style(Padding()),
