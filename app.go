3baefd074b5d5bc649b35c8927f5e1a01bf7287b
Torben Schinke <torben.schinke@worldiety.de>
2020-04-09 17:48:47 +0200

    added primary color example

--- app.go
+++ app.go
@@ -12,11 +12,11 @@ import (
 	"github.com/worldiety/wtk-example/demo/notfound"
 	"github.com/worldiety/wtk-example/demo/textarea"
 	"github.com/worldiety/wtk-example/demo/textfield"
+	"github.com/worldiety/wtk-example/demo/theme"
 	"github.com/worldiety/wtk-example/demo/topappbar"
 	"github.com/worldiety/wtk-example/demo/typography"
 	"github.com/worldiety/wtk/theme/material/icon"
 	"path/filepath"
-	"strconv"
 	"time"
 )
 
@@ -28,38 +28,6 @@ func NewApp() *App {
 	a := &App{}
 	a.Application = NewApplication(a)
 	return a
-}
-
-type MyCustomComponent struct {
-	*VStack
-	text1   *Text
-	text2   *Text
-	counter int
-}
-
-func NewMyCustomComponent() *MyCustomComponent {
-	c := &MyCustomComponent{}
-
-	NewVStack().AddViews(
-		NewText("hello world 2").Self(&c.text1).Style(PadLeft(12), ForegroundColor(RGBA(255, 0, 0, 255))),
-		NewText("a second text line").Self(&c.text2),
-		NewButton("press me").AddClickListener(func(v View) {
-			c.counter++
-			c.text1.Set("text 1: clicked " + strconv.Itoa(c.counter))
-			c.text2.Set("text 2: clicked " + strconv.Itoa(c.counter))
-
-			text := NewText("your click no " + strconv.Itoa(c.counter))
-			c.VStack.AddViews(text)
-		}),
-		NewButton("button 2").AddClickListener(func(v View) {
-			v.(*Button).SetEnabled(false)
-		}).SetStyleKind(Raised),
-		NewButton("button 3").SetStyleKind(Outlined).AddIcon(icon.AccessAlarm, Trailing),
-		NewButton("button 4").SetStyleKind(Unelevated).AddIcon(icon.AddAlert, Leading),
-		NewButton("button 5").SetStyleKind(Default).AddIcon(icon.Call, Leading).AddIcon(icon.Close, Trailing),
-	).Self(&c.VStack)
-
-	return c
 }
 
 func (a *App) WithDrawer(f func(q Query) View) func(Query) View {
@@ -113,5 +81,6 @@ func (a *App) Start() {
 	a.Route(drawer.Path, a.WithDrawer(drawer.FromQuery))
 	a.Route(list.Path, a.WithDrawer(list.FromQuery))
 	a.Route(link.Path, a.WithDrawer(link.FromQuery))
+	a.Route(theme.Path, a.WithDrawer(theme.FromQuery))
 	a.Application.Start()
 }
---
+++ demo/theme/component.go
@@ -0,0 +1,75 @@
+package theme
+
+import (
+	. "github.com/worldiety/wtk"
+)
+
+const Path = "/demo/theme"
+
+type ContentView struct {
+	*VStack
+	btn *Button
+}
+
+func NewContentView() *ContentView {
+	view := &ContentView{}
+	colorButtons := NewGroup()
+	for name, color := range Colors {
+		myColor := color
+		colorButtons.AddViews(
+			NewButton(name).
+				AddClickListener(func(v View) {
+					Theme().SetPrimaryColor(myColor)
+				}).Style(ForegroundColor(myColor)),
+		)
+	}
+
+	view.VStack = NewVStack().AddViews(
+		NewText("Theme").Style(Font(Headline1)),
+		NewText("Colors are important to increase the recognition factor "+
+			" and to focus on sensitive actions.").Style(Font(Body)),
+		colorButtons,
+		NewCode(GoSyntax, code),
+	)
+	return view
+}
+
+func FromQuery(Query) View {
+	return NewContentView()
+}
+
+const code = `package theme
+
+import (
+	. "github.com/worldiety/wtk"
+)
+
+const Path = "/demo/theme"
+
+type ContentView struct {
+	*VStack
+	btn *Button
+}
+
+func NewContentView() *ContentView {
+	view := &ContentView{}
+	colorButtons := NewGroup()
+	for name, color := range Colors {
+		myColor := color
+		colorButtons.AddViews(
+			NewButton(name).
+				AddClickListener(func(v View) {
+					Theme().SetPrimaryColor(myColor)
+				}).Style(ForegroundColor(myColor)),
+		)
+	}
+
+	view.VStack = NewVStack().AddViews(
+		NewText("Theme").Style(Font(Headline1)),
+		NewText("Colors are important to increase the recognition factor "+
+			" and to focus on sensitive actions.").Style(Font(Body)),
+		colorButtons,
+		NewCode(GoSyntax, code),
+	)
+	return view
+}`
