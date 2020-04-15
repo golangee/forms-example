be1164d4b1f2559701d359f6ebd6d4c206a1e349
Torben Schinke <torben.schinke@worldiety.de>
2020-04-15 16:34:37 +0200

    updated picker example

--- demo/picker/component.go
+++ demo/picker/component.go
@@ -18,13 +18,23 @@ func NewContentView() *ContentView {
 		NewText("Picker").Style(Font(Headline1)),
 		NewText("A picker allows to select exactly one option from a fixed list.").Style(Font(Body)),
 		NewPicker().
-			SetCaption("select your meal").
+			SetLabel("select your meal").
 			SetOptions("", "bread", "butter", "salt").
 			SetSelected(1).
 			SetSelectListener(func(v *Picker) {
 				ShowMessage(view, "you selected index "+strconv.Itoa(v.Selected()))
-			}),
-		NewPicker().SetCaption("disabled").SetEnabled(false),
+			}).
+			Style(Repel()),
+
+		NewPicker().SetLabel("disabled").SetHelper("you can't do anything").SetEnabled(false).
+			Style(Repel()),
+
+		NewPicker().
+			SetLabel("select invalid").
+			SetHelper("you got something wrong").
+			SetInvalid(true).
+			SetOptions("a", "b", "c").
+			Style(Repel()),
 
 		NewCode(GoSyntax, code),
 	)
@@ -35,4 +45,45 @@ func FromQuery(Query) View {
 	return NewContentView()
 }
 
-const code = ``
+const code = `package picker
+
+import (
+	. "github.com/worldiety/wtk"
+	"strconv"
+)
+
+const Path = "/demo/picker"
+
+type ContentView struct {
+	*VStack
+	btn *Button
+}
+
+func NewContentView() *ContentView {
+	view := &ContentView{}
+	view.VStack = NewVStack().AddViews(
+		NewText("Picker").Style(Font(Headline1)),
+		NewText("A picker allows to select exactly one option from a fixed list.").Style(Font(Body)),
+		NewPicker().
+			SetLabel("select your meal").
+			SetOptions("", "bread", "butter", "salt").
+			SetSelected(1).
+			SetSelectListener(func(v *Picker) {
+				ShowMessage(view, "you selected index "+strconv.Itoa(v.Selected()))
+			}).
+			Style(Repel()),
+
+		NewPicker().SetLabel("disabled").SetHelper("you can't do anything").SetEnabled(false).
+			Style(Repel()),
+			
+		NewPicker().
+			SetLabel("select invalid").
+			SetHelper("you got something wrong").
+			SetInvalid(true).
+			SetOptions("a", "b", "c").
+			Style(Repel()),
+
+		NewCode(GoSyntax, code),
+	)
+	return view
+}`
